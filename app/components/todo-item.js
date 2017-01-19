05b7df37f83b607b93b6c588dfd28517777e9e51
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-19 18:39:02 -0500

    need to debug clear completed button.

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -4,9 +4,23 @@ export default Ember.Component.extend({
   tagName: 'li',
   classNameBindings: ['editing'],
   editing: false,
+  // this key is taking the editing binding above it& giving it a boolean value, if it was a string it would give it a string value
   actions: {
     editTodo() {
       this.toggleProperty('editing');
+    },
+    submitTodo() {
+      const todo = this.get("zebra");
+      if (todo.get("title") === "") {
+        todo.destroyRecord().then(() => {
+          this.toggleProperty("editing");
+        });
+      } else {
+        this.toggleProperty("editing");
+      }
+    },
+    deleteTodo() {
+      this.get("zebra").destroyRecord();
     }
   }
 });
